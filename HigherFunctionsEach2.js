45571ba48f4d0e9af5e9b730e52d18620aa50a7e
DESKTOP-BHGTEQR\Hussein <hmtirawi@gmail.com>
2018-02-14 09:55:35 +0200

    higherFunctions2

--- HigherFunctionsEach2.js
+++ HigherFunctionsEach2.js
@@ -5,15 +5,26 @@ var x = [{name : 'Jon',age : 45}, {name : 'Ali', age : 28},
 {name :'Omar', age :17},{name :'Ola', age :37}, {name 'Salwa', age : 22}];
 pName(x); = > ['Jon', 'Omar', 'salwa']
 */
-	function each(array, func) {
-	  for (var i = 0; i < array.length; i++) {
-	    func(array[i], i);
-	  }
+
+
+var argument = [{name : 'Jon',age : 45}, {name : 'Ali', age : 28},{name :'Ola', age :37}, {name:'Salwa', age : 22}];
+
+function each(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		func(array[i], i);
 	}
-    
-	function pName(argument) {
-		// your code is here
-	}
+}
+
+function pName(argument) {
+	each(argument,function(arg,i){
+		
+		if(i %2 === 0){
+			console.log(arg.name);
+		}
+
+
+	})
+}
 
 
 /*
@@ -28,21 +39,23 @@ printValue(obj_1); =>
     Salim
     15
     079-0000000
-*/
+    */
     function each(coll, func) {
-        if (Array.isArray(coll)) {
-            for (var i = 0; i < coll.length; i++) {
-                func(coll[i], i);
-            }
-        }
-        else {
-            for (var key in coll) {
-                func(coll[key], key);
-            }
-        }
+    	if (Array.isArray(coll)) {
+    		for (var i = 0; i < coll.length; i++) {
+    			func(coll[i], i);
+    		}
+    	}
+    	else {
+    		for (var key in coll) {
+    			func(coll[key], key);
+    		}
+    	}
     }
     
-    
-	function printValue(obj) {
-		// your code is here
-	}
+    function printValue(obj) {
+    	each(obj,function(i){
+    		console.log(i)
+
+    	})
+    }
