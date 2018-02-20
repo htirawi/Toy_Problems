61e856439cf975f37997b0ef35dc289566425448
Hussein <hmtirawi@gmail.com>
2018-02-20 09:40:58 +0200

    ObjectOriented

--- Oop.js
+++ Oop.js
@@ -2,29 +2,63 @@
 // Refactor the code as an MakeGame class that shares its methods across different instances.
 
 
-function randInt(n) {
-    return Math.floor(Math.random() * (n + 1));
-}
+// function randInt(n) {
+// 	return Math.floor(Math.random() * (n + 1));
+// }
+
+// function makeGame(upperbound){
+// 	var upper=upperbound;
+// 	var rand=randInt(upper)
+// 	var counter=0;
+// 	return {
+// 		guessMyNumber:function(n){
+// 			counter++;
+// 			if (n > upper) {
+// 				return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+// 			} else if (n === rand) {
+// 				return "You guessed my number!";
+// 			}
+// 			return "Nope! That wasn't it!";
+// 		},
+// 		giveUp:function(){
+// 			return rand;
+// 		},
+// 		numOfGuesses:function(){
+// 			return counter;
+// 		}
+// 	}
+// }
+
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+	// debugger;
+	var obj = {};
+	obj.upper  = upperbound;
+	obj.rand = randInt(obj.upper);
+	obj.counter = 0;
+	obj.guessMyNumber = guessMyNumber;
+	obj.giveUp = giveUp;
+	obj.numOfGuesses = numOfGuesses;
+}
+
+var randInt = function(n){
+	return Math.floor(Math.random() * (n + 1));
+}
+
+var guessMyNumber = function(n){
+	counter++;
+	if (n > this.upper) {
+		return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+	} else if (n === this.rand) {
+		return "You guessed my number!";
+	}
+	return "Nope! That wasn't it!";
+}
+
+var giveUp=function(){
+	return this.rand;
+}
+
+var numOfGuesses = function(){
+	return counter;
 }
