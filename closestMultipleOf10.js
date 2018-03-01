8a7aabccfb925c4e49f21b5cf702820e3cbd94a0
Hussein <hmtirawi@gmail.com>
2018-03-01 09:34:08 +0200

    clseset number to 10

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -3,6 +3,12 @@
 	// 22 ===> 20
 	// 25 ===> 30
 	// 37 ===> 40
- function closestMultipleOf10(num) {
- 	//your code is here
- }
+	function closestMultipleOf10(num) {
+		var modu = num%10;
+		if(modu<5){
+			return(num-modu);
+		}
+		else{
+			return(Math.ceil(num/10)*10)
+		}
+	}
